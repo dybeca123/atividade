e5ad0dcd8d5887cff9dbbcef26475d607ed33451
dybeca123 <103453892+dybeca123@users.noreply.github.com>
2022-05-23 16:45:42 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,68 @@
+//querySelector/PAULO
+
+var html = document.querySelector('#primeiro-aluno')
+
+var notahtml = html.querySelector('.info-notahtml');
+//console.log(valorPeso)
+
+var html = parseInt(notahtml.textContent); 
+console.log(html)
+
+var nome = html.querySelector ('info-nome')
+var html = parseInt(valorHtml.textContent);
+console.log (html)
+
+var valorIMC = parseInt(html+css+javaS) / 3
+
+var subIMC = html.querySelector('info-imc')
+subIMC.textContent = valorIMC 
+
+
+// CSS
+
+var css = document.querySelector ('#segundo-aluno')
+
+var notaCss = css.querySelector('#info-notacss');
+//console.log (valorCss)
+
+var css = valorCss = css.querySelector ('info-valorcss')
+var css = parseInt(valorCss.textContent);
+console.log (css)
+
+// JavaS
+
+var javaS = document.querySelector('#terceiro-aluno')
+
+var javaS = javaS.querySelector ('info-notajavaS');
+//console.log (valorJavaS)
+
+var javaS = parseInt(valorjavaS.textContent);
+console.log (javaS)
+
+var valorJavaS = javaS.querySelector ('info-valor-javaS')
+var javaS = valorJavaS.textContent;
+console.log (javaS)
+
+// Aluno 4
+
+var aluno4 = document.querySelector('#quarto-aluno')
+
+var aluno4 = javaS.querySelector ('info-aluno4');
+//console.log (javaS)
+
+var javaS = parseInt(valorjavaS.textContent);
+console.log (javaS)
+
+var valorJavaS = aluno4.querySelector ('info-valor-javaS')
+var javaS = (aluno4.textContent);
+console.log (aluno4)
+
+
+var valorIMC = html+css+javaS / 3
+
+var subIMC = html.querySelector('info-imc')
+subIMC.textContent = valorIMC 
+
+
+var valorIMC =  media / (html+css+javaS)
+console.log (valorIMC)  
